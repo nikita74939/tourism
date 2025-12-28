1e7d719f18cc28112fc2db6511f882a0d00704a8
nikita74939 <itanikita20005@gmail.com>
2025-12-28 12:12:29 +0700

    make routes

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tourism/controllers/splash_controller.dart';
+import '../../routes/app_routes.dart';
+import '../../storage/local_storage.dart';
+import '../../utils/constants.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+  final _storage = LocalStorage();
 
   @override
   Widget build(BuildContext context) {
-    Get.put(SplashController());
+    Future.delayed(Duration(seconds: 2), () {
+      if (_storage.isLoggedIn()) {
+        Get.offAllNamed(AppRoutes.mainNavigation);
+      } else {
+        Get.offAllNamed(AppRoutes.login);
+      }
+    });
 
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
       body: Center(
-        child: Text(
-          'Tourism',
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.flight_takeoff, size: 100, color: Colors.white),
+            SizedBox(height: 24),
+            Text(
+              AppConstants.appName,
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Explore the World',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white70,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,12 @@
-abstract class Routes {
-  static const splash = '/';
-  static const login = '/login';
-  static const home = '/home';
+class AppRoutes {
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String mainNavigation = '/main';
+  static const String explore = '/explore';
+  static const String map = '/map';
+  static const String planner = '/planner';
+  static const String addItinerary = '/add-itinerary';
+  static const String profile = '/profile';
 }
